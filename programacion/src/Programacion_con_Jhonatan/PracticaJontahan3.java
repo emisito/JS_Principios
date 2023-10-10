361f9df36cf26ce4c96dc6bd1017ef827fe890be
Emanuel <emisitokomunica@gmail.com>
2023-10-10 17:39:26 +0200

    Refrescando paracticajontathan3

--- programacion/src/Programacion_con_Jhonatan/PracticaJontahan3.java
+++ programacion/src/Programacion_con_Jhonatan/PracticaJontahan3.java
@@ -8,13 +8,13 @@ public class PracticaJontahan3 {
 		
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Introduce cualquier número y yo te dire si es par o impar :)");
-			int num= sc.nextInt();
-			int num1= num % 2;
+			int num1= sc.nextInt();
+			int num2= num1 % 2;
 			
-		if(num1 == 0) {
-			System.out.println("Este número " + num + " es par ");
+		if(num2 == 0) {
+			System.out.println("Este número " + num1 + " es par ");
 		}else {
-			System.out.println("Este número " + num + " es impar ");
+			System.out.println("Este número " + num1 + " es impar ");
 		}
 			
 	}
