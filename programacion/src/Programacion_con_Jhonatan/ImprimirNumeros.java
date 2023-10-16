ace3c634bb3910c5f75173c6a24f1ef31eb27e3c
Emanuel <emisitokomunica@gmail.com>
2023-10-16 19:30:48 +0200

    un simple for

---
+++ programacion/src/Programacion_con_Jhonatan/ImprimirNumeros.java
@@ -0,0 +1,13 @@
+package Programacion_con_Jhonatan;
+
+public class ImprimirNumeros {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 100; i++){
+			System.out.print(i + ",");
+		}
+		
+	}
+
+}
