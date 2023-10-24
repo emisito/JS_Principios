90d18b72067f492d4dea88a5261e0f880c0adaac
Emanuel <emisitokomunica@gmail.com>
2023-10-24 20:36:48 +0200

    mas cositas

---
+++ programacion/src/Programacion_con_Jhonatan/PracticaSwich2.java
@@ -0,0 +1,33 @@
+package Programacion_con_Jhonatan;
+
+import java.util.Scanner;
+
+public class PracticaSwich2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Ingresa un número");
+		int num1 = scn.nextInt();
+		System.out.println("Ingresa un segundo número");
+		int num2 = scn.nextInt();
+		System.out.println("Intoduce un operador");
+		char operador = scn.next().charAt(0);
+		
+		switch (operador){
+		case '+':
+			System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+			break;
+		case '-':
+			System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+			break;
+		case '*':
+			System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+			break;
+		case '/':
+			System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+			break;
+		}
+	}
+
+}
---
+++ programacion/src/Programacion_con_Jhonatan/PracticaSwich3.java
@@ -0,0 +1,34 @@
+package Programacion_con_Jhonatan;
+
+import java.util.Scanner;
+
+public class PracticaSwich3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Indique que mondea desea cambiar E para euros y D para dolares");
+		char moneda = scn.next().charAt(0);
+		
+		System.out.println("Ingrese la cantidad que desea cambiar");
+		double num1 = scn.nextDouble();
+		
+		System.out.println("Desea cambiarlo a la otra moneda si es que si introduzca S y si no N");
+		char respuesta = scn.next().charAt(0);
+		
+		//Tasa de cambio
+		
+		double tasa = 1.06;
+		
+		if (respuesta == 's') {
+			switch (moneda){
+				case 'e':
+					System.out.println("La cantidad de " + num1 + " € en dolares seria de " + (num1 * tasa) + "$");
+			}
+		}
+
+		
+	}
+
+}
