262a2d0f9356f3e062a7675970a21c5907cfd0d0
Emanuel <emisitokomunica@gmail.com>
2023-10-27 20:23:01 +0200

    Primer ejercicio de los metodos jeje

---
+++ programacion/src/PracticasMetodos/practicaMetodos1.java
@@ -0,0 +1,33 @@
+package PracticasMetodos;
+
+import java.util.Scanner;
+
+public class practicaMetodos1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Ingresa una cadena de texto");
+		String cadena = scn.nextLine();
+		
+		int cantidadA = contarAs(cadena);
+		
+		System.out.println("La cadena tiene " + cantidadA + " A/a");
+	
+	}
+	
+	public static int contarAs(String cadena) {
+	    int contador = 0;
+	    for (int i = 0; i < cadena.length(); i++) {
+	        if (cadena.charAt(i) == 'a') {
+	            contador++;
+	        } else if (cadena.charAt(i) == 'A') {
+	        	contador++;
+	        }
+	    }
+	    return contador;
+	}
+	
+}
+
+
